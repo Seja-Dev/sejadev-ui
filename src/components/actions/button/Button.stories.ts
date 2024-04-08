b3a6ce4c564ec2c3bb48a2ce6fb11c20a6feb666
NJesus144 <nalbert-edm@outlook.com>
2024-04-08 16:22:52 -0300

    component button created

--- src/components/actions/button/Button.stories.ts
+++ src/components/actions/button/Button.stories.ts
@@ -3,14 +3,59 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Button } from './Button'
 
 const meta: Meta<typeof Button> = {
-  component: Button
+  component: Button,
+  argTypes: {
+    onClick: { action: 'clicked' }
+  }
 }
 export default meta
 
 type Story = StoryObj<typeof Button>
 
-export const Basic: Story = {
+export const Primary: Story = {
   args: {
-    label: 'Basic button'
+    label: 'Button'
   }
 }
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    label: 'Disabled'
+  }
+}
+
+export const Skeleton: Story = {
+  args: {
+    skeleton: true
+  }
+}
+
+export const Loading: Story = {
+  args: {
+    loading: true,
+    label: 'Loading'
+  }
+}
+
+export const Sizes: Story = {
+  argTypes: {
+    sizes: {
+      control: {
+        type: 'select',
+        options: ['btn-xs', 'btn-sm', 'btn-md', 'btn-lg']
+      }
+    }
+  }
+}
+
+// export const Primary = Template.bind({})
+// Primary.args = {
+//   label: 'Button'
+// }
+
+// export const Disabled = Template.bind({})
+// Disabled.args = {
+//   disabled: true,
+//   label: 'Disabled'
+// }
--- src/components/actions/button/Button.tsx
+++ src/components/actions/button/Button.tsx
@@ -1,14 +1,35 @@
-/**
- * Primary UI component for user interaction
- */
-
 interface IButton {
   label: string
+  fullWidth?: boolean
+  onClick?: () => void
+  loading?: boolean
+  className?: string
+  skeleton?: boolean
+  disabled?: boolean
+  sizes?: 'btn-xs' | 'btn-sm' | 'btn-md' | 'btn-lg'
 }
 
-export function Button({ label }: IButton) {
+export const Button: React.FC<IButton & React.HTMLProps<HTMLButtonElement>> = ({
+  label,
+  onClick,
+  loading,
+  className,
+  fullWidth,
+  disabled,
+  skeleton,
+  sizes
+}: IButton) => {
+
+  if (skeleton) {
+    return <div className={`skeleton ${fullWidth && 'w-full'} ${className}`} />
+  }
+
   return (
-    <button className="btn btn-primary uppercase text-white" type="button">
+    <button
+      className={`btn btn-primary ${fullWidth && 'w-full'} ${loading && 'loading'} uppercase ${className} ${sizes}`}
+      type="button"
+      disabled={disabled || loading}
+      onClick={onClick}>
       {label}
     </button>
   )
