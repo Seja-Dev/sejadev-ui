f552a7576f2e5973e03bf7443ea962894e362e9b
NJesus144 <nalbert-edm@outlook.com>
2024-04-16 15:42:22 -0300

    change style color

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -16,7 +16,7 @@ export function Modal({ isOpen, onClose, children }: IModal) {
         onRequestClose={onClose}
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[700px] mx-auto p-0"
         ariaHideApp={false}>
-        <div className="bg-[#141316] text-white p-5 min-h-24">
+        <div className="bg-base-100 p-5 min-h-24">
           <div
             onClick={onClose}
             className="absolute top-3.5 right-5 text-2xl font-bold cursor-pointer">
