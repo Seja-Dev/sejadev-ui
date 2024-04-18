9180e368ac981bfcb48cfb031309330c0152f2aa
NJesus144 <nalbert-edm@outlook.com>
2024-04-18 12:47:20 -0300

    remove fragment

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -9,23 +9,20 @@ export interface IModal {
 }
 
 export function Modal({ isOpen, onClose, children }: IModal) {
-
   return (
-    <>
-      <ReactModal
-        isOpen={isOpen}
-        onRequestClose={onClose}
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[700px] mx-auto p-0"
-        ariaHideApp={false}>
-        <div className="bg-base-100 p-5 min-h-24">
-          <div
-            onClick={onClose}
-            className="absolute top-3.5 right-5 text-2xl font-bold cursor-pointer">
-            &times;
-          </div>
-          {children}
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[700px] mx-auto p-0"
+      ariaHideApp={false}>
+      <div className="bg-base-100 p-5 min-h-24">
+        <div
+          onClick={onClose}
+          className="absolute top-3.5 right-5 text-2xl font-bold cursor-pointer">
+          &times;
         </div>
-      </ReactModal>
-    </>
+        {children}
+      </div>
+    </ReactModal>
   )
 }
