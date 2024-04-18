d0cdc6e14b85f5687e6fce831fe3a0986d418d9d
NJesus144 <nalbert-edm@outlook.com>
2024-04-18 12:04:00 -0300

    created Accordion component

---
+++ src/components/accordion/Accordion.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Accordion } from './Accordion'
+
+const meta: Meta<typeof Accordion> = {
+  component: Accordion
+}
+export default meta
+
+type Story = StoryObj<typeof Accordion>
+
+export const Basic: Story = {
+  args: {
+    title: 'Módulo 1 - O Início',
+    subtitle: '3 aulas',
+    children: 'Configuração do Ambiente'
+
+  }
+}
+
+export const Disabled: Story = {
+  args: {
+    title: 'Disabled accordion',
+    disabled: true
+  }
+}
+
+export const Transparent: Story = {
+  args: {
+    title: 'Transparent accordion',
+    transparent: true
+  },
+}
---
+++ src/components/accordion/Accordion.tsx
@@ -0,0 +1,66 @@
+import { useState, ReactNode } from 'react'
+
+interface IAccordion {
+  firstState?: boolean
+  title?: string
+  subtitle?: string
+  disabled?: boolean
+  children?: ReactNode
+  transparent?: boolean
+}
+
+export function Accordion({
+  firstState = false,
+  title,
+  subtitle,
+  disabled,
+  children,
+  transparent
+}: IAccordion) {
+  const [open, setOpen] = useState(disabled ? false : firstState)
+
+  return (
+    <div className={`${transparent ? 'bg-transparent' : 'bg-[#141316]'} p-4 rounded-lg`}>
+      <div
+        onClick={() => !disabled && setOpen(!open)}
+        className="flex justify-between items-center w-full cursor-pointer">
+        <div>
+          <h4 className="font-bold">{title}</h4>
+          {subtitle && <p className="text-sm">{subtitle}</p>}
+        </div>
+        <svg
+          className="fill-white shrink-0 ml-8"
+          width="16"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg">
+          <rect
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+            className={`transform origin-center transition duration-200 ease-out ${
+              open && '!rotate-180'
+            }`}
+          />
+          <rect
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
+              open && '!rotate-180'
+            }`}
+          />
+        </svg>
+      </div>
+      <div
+        className={`grid overflow-hidden transition-all duration-300 ease-in-out ${
+          open ? 'grid-rows-[1fr] opacity-100 py-4' : 'grid-rows-[0fr] opacity-0'
+        }`}>
+        <div className={`overflow-hidden ${transparent ? 'bg-transparent' : 'bg-[#141316]'}`}>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
