d7aa71e5c0306e3e4a1405ba326081b21cedd30f
NJesus144 <nalbert-edm@outlook.com>
2024-05-08 12:11:35 -0300

    update Input component to use new color theme

--- src/components/form/input/Input.tsx
+++ src/components/form/input/Input.tsx
@@ -30,8 +30,7 @@ export function Input({
   skeletonClassName,
   ...props
 }: IInput) {
-  
-  if (skeleton) return <div className={`skeleton w-full h-14 rounded-md ${skeletonClassName}`} />
+  if (skeleton) return <div className={`skeleton ${fullWidth && 'w-full'} h-14 rounded-md ${skeletonClassName}`} />
 
   return (
     <div className="relative flex flex-col">
@@ -42,8 +41,8 @@ export function Input({
         ${
           error
             ? 'border-error'
-            : 'border-neutral focus:border-primary focus:ring-1 focus:ring-primary'
-        } bg-base-100 px-5 pt-5 pb-2 
+            : 'border-input-background focus:border-primary focus:ring-1 focus:ring-primary'
+        } bg-input-background px-5 pt-5 pb-2 
         ${fullWidth && 'w-full'} `}
         name={name}
         value={value}
@@ -51,7 +50,7 @@ export function Input({
         {...props}
       />
       <label
-        className={`absolute text-[#898989] top-1 left-5 text-xs transition-all pointer-events-none peer-placeholder-shown:text-base peer-placeholder-shown:top-4 peer-focus:top-1 peer-focus:text-xs`}>
+        className={`absolute text-common-grey20 top-1 left-5 text-xs transition-all pointer-events-none peer-placeholder-shown:text-base peer-placeholder-shown:top-4 peer-focus:top-1 peer-focus:text-xs`}>
         {required ? `${label} *` : label}
       </label>
       {error && (
