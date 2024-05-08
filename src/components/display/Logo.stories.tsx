4605d864e0c6a29a2100f750627271ddc66e2fb1
NJesus144 <nalbert-edm@outlook.com>
2024-05-08 13:19:00 -0300

    add white and symbol colored Logo options stories

--- src/components/display/Logo.stories.tsx
+++ src/components/display/Logo.stories.tsx
@@ -11,14 +11,33 @@ type Story = StoryObj<typeof Logo>
 
 export const Basic: Story = {
   args: {
-    type: 'colored',
+    type: 'colored'
   }
 }
 
+export const White: Story = {
+  args: {
+    type: 'white'
+  }
+}
+
+export const SymbolColored: Story = {
+  args: {
+    type: 'symbol_colored',
+    className: 'w-20 h-20'
+  }
+}
+
+export const SymbolWhite: Story = {
+  args: {
+    type: 'symbol_white',
+    className: 'w-20 h-20',
+    skeletonClassName: 'w-14 h-20 rounded-3xl'
+  }
+}
 
 export const Skeleton: Story = {
   args: {
-    skeleton: true,
+    skeleton: true
   }
 }
-
