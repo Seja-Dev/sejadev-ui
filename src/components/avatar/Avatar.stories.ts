e2bab132c8d20c7befcea8ad17af6d28def586dc
NJesus144 <nalbert-edm@outlook.com>
2024-05-09 17:38:18 -0300

    remove className from avatar stories

--- src/components/avatar/Avatar.stories.ts
+++ src/components/avatar/Avatar.stories.ts
@@ -12,20 +12,17 @@ type Story = StoryObj<typeof Avatar>
 export const WithImage: Story = {
   args: {
     src: 'https://cdn-icons-png.flaticon.com/512/3135/3135768.png',
-    className: 'w-10 h-10'
   }
 }
 
 export const WithoutImage: Story = {
   args: {
     text: 'Aluno Teste',
-    className: 'w-10 h-10 text-xl text-white'
   }
 }
 
 export const Skeleton: Story = {
   args: {
     skeleton: true,
-    skeletonClassName: 'w-10 h-10 rounded-full'
   }
 }
