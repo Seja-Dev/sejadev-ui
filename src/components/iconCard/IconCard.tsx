03ad45a2233922f94a93bbb3ce93000d01dc2cab
NJesus144 <nalbert-edm@outlook.com>
2024-05-09 17:28:47 -0300

    remove next router and add onClick prop

--- src/components/iconCard/IconCard.tsx
+++ src/components/iconCard/IconCard.tsx
@@ -1,5 +1,3 @@
-import Router from 'next/router'
-
 interface IIconCard {
   children: React.ReactNode
   title: string
@@ -7,17 +5,16 @@ interface IIconCard {
   skeleton?: boolean
   skeletonClassName?: string
   className?: string
+  onClick?: () => void
 }
 
-export function IconCard({ children, title, link, skeleton, className, skeletonClassName }: IIconCard) {
+export function IconCard({ children, title, skeleton, className, skeletonClassName, onClick }: IIconCard) {
   if (skeleton) return <div className={`skeleton md:max-w-[299px] h-32 rounded-xl ${skeletonClassName}`}></div>
 
   return (
     <div
       className={`md:max-w-[299px] bg-neutral-900 flex flex-col items-center rounded-xl p-5 gap-2 cursor-pointer hover:shadow-primary-all ${className}`}
-      onClick={() => {
-        Router.push(link)
-      }}>
+      onClick={onClick}>
       {children}
       <h3 className="font-bold text-xl">{title}</h3>
     </div>
