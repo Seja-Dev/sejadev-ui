ae519dcfa29c8b7ece42d9491386cca209d28ea4
NJesus144 <nalbert-edm@outlook.com>
2024-05-09 16:37:49 -0300

    add previously removed features and replaced prop label with children

--- src/components/actions/button/Button.tsx
+++ src/components/actions/button/Button.tsx
@@ -5,16 +5,52 @@ interface IButton {
   skeletonClassName?: string
   loading?: boolean
   disabled?: boolean
-  label?: string
   onClick?: () => void
   btnSize?: 'xs' | 'sm' | 'md' | 'lg'
   variant?: 'primary' | 'secondary' | 'accent' | 'neutral' | 'ghost' | 'link' | 'success' | 'error'
 }
 
-export function Button({ label }: IButton) {
+export function Button({
+  skeleton,
+  fullWidth,
+  className,
+  skeletonClassName,
+  loading,
+  btnSize = 'md',
+  disabled,
+  children,
+  onClick,
+  variant = 'primary'
+}: IButton & React.HTMLProps<HTMLButtonElement>) {
+  if (skeleton)
+    return (
+      <div className={`skeleton ${fullWidth && 'w-full'} h-12 rounded-md ${skeletonClassName}`} />
+    )
+
+  const btnVariant = {
+    primary: 'btn-primary text-white',
+    secondary: 'btn-secondary text-white',
+    accent: 'btn-accent text-white',
+    neutral: 'btn-neutral text-white',
+    ghost: 'btn-ghost text-primary',
+    link: 'btn-link text-primary',
+    success: 'btn-success text-white',
+    error: 'btn-error text-white'
+  }
+
+  const btnSizeVariant = {
+    xs: 'btn-xs',
+    sm: 'btn-sm',
+    md: 'btn-md',
+    lg: 'btn-lg'
+  }
   return (
-    <button className="btn btn-primary uppercase text-white" type="button">
-      {label}
+    <button
+      className={`btn ${fullWidth && 'w-full'} uppercase ${btnVariant[variant]} ${btnSizeVariant[btnSize]} ${className}`}
+      type="button"
+      disabled={disabled || loading}
+      onClick={onClick}>
+      {loading ? <span className="loading loading-spinner"></span> : children}
     </button>
   )
 }
