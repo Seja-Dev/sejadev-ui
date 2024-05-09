26172e8caa35721ca1de45755499cd20f6143de0
NJesus144 <nalbert-edm@outlook.com>
2024-05-09 16:33:11 -0300

    change name component from LessonList to Lesson

--- src/components/LessonList/Lesson.stories.ts
+++ src/components/LessonList/Lesson.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { LessonStyle } from './LessonList'
+import { LessonStyle } from './Lesson'
 
 const meta: Meta<typeof LessonStyle> = {
   component: LessonStyle
