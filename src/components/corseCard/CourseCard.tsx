bf3bc8585210127e6cbfad268ae00128ead43c52
Nalbert de Jesus <94402903+NJesus144@users.noreply.github.com>
2024-05-10 10:20:15 -0300

    Ds 48 (#14)
    * config files
    
    * created badge component
    
    * changed style classes
    
    * created courseCard
    
    * changes style class badge
    
    * add skeletonClassName props
    
    * add default value skeleton and skeletonClassName props
    
    * remove router and add onClick prop

---
+++ src/components/corseCard/CourseCard.tsx
@@ -0,0 +1,62 @@
+import { Badge } from '../badge/Badge'
+
+interface ICourseCard {
+  title: string
+  description?: string
+  url: string
+  level: number
+  tags?: string[]
+  skeleton?: boolean
+  skeletonClassName?: string
+  className?: string
+  onClick?: () => void
+}
+
+interface levelType {
+  [key: number]: string
+}
+
+const LEVEL_DICT: levelType = {
+  0: 'Iniciante',
+  1: 'Intermediário',
+  2: 'Intermediário-avançado',
+  3: 'Avançado',
+  4: 'Expert'
+}
+
+export function CourseCard({
+  title,
+  description,
+  level,
+  tags,
+  skeleton,
+  skeletonClassName,
+  className,
+  onClick
+}: ICourseCard) {
+  if (skeleton) return <div className={`skeleton md:w-96 h-32 ${skeletonClassName}`}></div>
+
+  return (
+    <div
+      className={`card w-fit md:w-96 shadow-xl cursor-pointer hover:shadow-primary-all ${className}`}
+      onClick={onClick}>
+      <div className={`card-body bg-neutral-900 rounded-2xl p-6`}>
+        <h2 className="card-title">{title}</h2>
+        <h4>
+          <span className="font-bold">Nível:</span>
+          <Badge variant="neutral" className="ml-1">
+            {LEVEL_DICT[level] || 'Indefinido'}
+          </Badge>
+        </h4>
+        <p className="my-2">{description}</p>
+        <div className="flex flex-wrap gap-1 items-start">
+          {tags?.map((str, i) => (
+            <Badge variant="neutral" key={`${i}-${str}`}>
+              {str}
+            </Badge>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
