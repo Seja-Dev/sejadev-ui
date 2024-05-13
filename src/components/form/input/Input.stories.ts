e9454647278c9366f73d671153bcc658d8a86947
Nalbert de Jesus <94402903+NJesus144@users.noreply.github.com>
2024-05-13 10:33:52 -0300

    Ds 40 (#6)
    * config files
    
    * created input component and mask with react-imask
    
    * changes the export type to the project default
    
    * style changed outline input
    
    * style change outline MaskedInput
    
    * removed maskedInput component and react-imask lib
    
    * remove error prop from input element
    
    * Add skeletonClassName prop to Input component and default value skeleton
    
    * update Input component to use new color theme

---
+++ src/components/form/input/Input.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Input } from './Input'
+
+const meta: Meta<typeof Input> = {
+  component: Input
+}
+
+export default meta
+type Story = StoryObj<typeof Input>
+
+export const Primary: Story = {
+  args: {
+    name: 'Nome',
+    label: 'Nome Completo',
+    skeletonClassName: 'w-96 h-12'
+  }
+}
+
+export const Error: Story = {
+  args: {
+    name: 'Nome',
+    label: 'Nome Completo',
+    error: true
+  }
+}
+
+export const Skeleton: Story = {
+  args: {
+    skeleton: true,
+    className: 'w-1/6 h-12 rounded-md'
+  }
+}
---
+++ src/components/form/input/Input.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+interface IInput {
+  name?: string
+  skeleton?: boolean
+  label?: string
+  fullWidth?: boolean
+  required?: boolean
+  value?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  error?: boolean
+  type?: string
+  className?: string
+  skeletonClassName?: string
+  disabled?: boolean
+}
+
+export function Input({
+  name,
+  skeleton,
+  fullWidth,
+  required,
+  label,
+  value,
+  onChange,
+  error,
+  type,
+  disabled,
+  className,
+  skeletonClassName,
+  ...props
+}: IInput) {
+  if (skeleton) return <div className={`skeleton ${fullWidth && 'w-full'} h-14 rounded-md ${skeletonClassName}`} />
+
+  return (
+    <div className="relative flex flex-col">
+      <input
+        disabled={disabled}
+        type={type}
+        className={`peer border-2 rounded-md placeholder-transparent disabled:opacity-40 outline-none ${className}
+        ${
+          error
+            ? 'border-error'
+            : 'border-input-background focus:border-primary focus:ring-1 focus:ring-primary'
+        } bg-input-background px-5 pt-5 pb-2 
+        ${fullWidth && 'w-full'} `}
+        name={name}
+        value={value}
+        placeholder=""
+        {...props}
+      />
+      <label
+        className={`absolute text-common-grey20 top-1 left-5 text-xs transition-all pointer-events-none peer-placeholder-shown:text-base peer-placeholder-shown:top-4 peer-focus:top-1 peer-focus:text-xs`}>
+        {required ? `${label} *` : label}
+      </label>
+      {error && (
+        <div className="label-text-alt my-2 text-error">
+          {`O campo ${name} é obrigatório!` || ''}
+        </div>
+      )}
+    </div>
+  )
+}
