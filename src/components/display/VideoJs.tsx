a6b5a5455cd7cc33594dcc1e7a4e4721444b1fe7
Nalbert de Jesus <94402903+NJesus144@users.noreply.github.com>
2024-05-14 22:00:05 -0300

    created video component (#19)
    * config files
    
    * changed style classes
    
    * add Accordion skeleton and className style prop
    
    * created texarea component
    
    * created video component
    
    * add video.js package
    
    * update package.json
    
    * update package-lock.json and tsconfig.json
    
    * update VideoJs component to add new props
    
    * removed unneccessary files
    
    * update VideoJs component URL prop
    
    * update package lock
    
    ---------
    
    Co-authored-by: Jose Fortes <vasco0007@gmail.com>

---
+++ src/components/display/VideoJs.tsx
@@ -0,0 +1,89 @@
+import { useRef, useEffect, useState, memo } from 'react'
+import videojs from 'video.js'
+import 'video.js/dist/video-js.css'
+
+import hlsQualitySelector from 'videojs-hls-quality-selector'
+videojs.registerPlugin('hlsQualitySelector', hlsQualitySelector)
+
+import 'videojs-contrib-quality-levels'
+import 'videojs-hls-quality-selector'
+import 'videojs-hotkeys'
+
+interface VideoJsProps {
+  url: string
+  onReady?: (player: videojs.Player) => void
+  responsive?: boolean
+  fluid?: boolean
+  playbackRates?: number[]
+  poster?: string
+}
+
+export const VideoJs: React.FC<VideoJsProps> = memo(
+  ({
+    onReady,
+    url,
+    responsive = true,
+    fluid = true,
+    playbackRates = [1, 1.2, 1.5, 1.8, 2],
+    poster = '/thumb.png'
+  }) => {
+    const videoRef = useRef<HTMLVideoElement>(null)
+    const [player, setPlayer] = useState<videojs.Player>()
+
+    useEffect(() => {
+      if (!player) {
+        const videoElement = videoRef.current
+        if (!videoElement) return
+
+        const videoJsOptions: videojs.PlayerOptions = {
+          autoplay: false,
+          controls: true,
+          responsive,
+          fluid,
+          playbackRates,
+          poster,
+          sources: [
+            {
+              src: url,
+              type: 'application/x-mpegURL'
+            }
+          ],
+          plugins: {
+            hotkeys: {
+              volumeStep: 0.1,
+              seekStep: 5,
+              enableModifiersForNumbers: false,
+              enableVolumeScroll: false
+            }
+          }
+        }
+
+        const newPlayer = videojs(videoElement, videoJsOptions, () => {
+          onReady && onReady(newPlayer)
+          setPlayer(newPlayer)
+          newPlayer.hlsQualitySelector({
+            displayCurrentQuality: true
+          })
+        })
+      }
+
+      return () => {
+        player && player.dispose()
+      }
+    }, [videoRef, player, onReady])
+
+    useEffect(() => {
+      if (player) {
+        player.src(url)
+      }
+    }, [url])
+
+    return (
+      <div data-vjs-player>
+        <video controls preload="auto" ref={videoRef} className="video-js vjs-big-play-centered" />
+      </div>
+    )
+  }
+)
+
+VideoJs.displayName = 'VideoJs'
