51d0f1b744bb739e4a349b1a3de5ddd5e183b9cd
NJesus144 <nalbert-edm@outlook.com>
2024-05-16 12:07:18 -0300

    reorganize component exports into folders

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,39 @@
 import './styles/globals.css'
 
-export * from './components/alert/Alert'
-
 //ACTIONS
 export * from './components/actions/button/Button'
+export * from './components/actions/dropdown/Dropdown'
+export * from './components/actions/modal/Modal'
+export * from './components/actions/modalWithButton/ModalWithButton'
+
+//CARDS
+export * from './components/cards/courseCard/CourseCard'
+export * from './components/cards/iconCard/IconCard'
+
+//DISPLAY
+export * from './components/display/accordion/Accordion'
+export * from './components/display/avatar/Avatar'
+export * from './components/display/badge/Badge'
+export * from './components/display/logo/Logo'
+export * from './components/display/videoJs/VideoJs'
+
+//FEEDBACK
+export * from './components/feedback/alert/Alert'
+
+//FORM
+export * from './components/form/input/Input'
+export * from './components/form/textarea/Textarea'
+export * from './components/form/textareaWithButton/TextareaWithButton'
+
+//LESSONLIST
+export * from './components/LessonList/chapter/Chapter'
+export * from './components/LessonList/lesson/Lesson'
+
+//TYPOGRAPHY
+export * from './components/typography/h1/H1'
+export * from './components/typography/h2/H2'
+export * from './components/typography/h3/H3'
+export * from './components/typography/h4/H4'
+export * from './components/typography/h5/H5'
+export * from './components/typography/h6/H6'
+export * from './components/typography/p/P'
