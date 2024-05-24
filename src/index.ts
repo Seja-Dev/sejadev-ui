04f99836341242e5861948edd28ee27bcda897c5
Jose Fortes <vasco0007@gmail.com>
2024-05-24 09:45:35 -0300

    remove player

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ export * from './components/display/accordion/Accordion'
 export * from './components/display/avatar/Avatar'
 export * from './components/display/badge/Badge'
 export * from './components/display/logo/Logo'
-export * from './components/display/player/Player'
 
 //FEEDBACK
 export * from './components/feedback/alert/Alert'
