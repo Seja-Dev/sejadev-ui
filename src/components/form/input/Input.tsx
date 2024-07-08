caf480d1f57dc2264c9fbad911fb73ea5802ee06
Nalbert <nalbert-edm@outlook.com>
2024-07-08 15:53:54 -0300

    add ref to InputMask component

--- src/components/form/input/Input.tsx
+++ src/components/form/input/Input.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
+
 import InputMask from 'react-input-mask'
 
 interface IInput {
@@ -17,53 +18,59 @@ interface IInput {
   disabled?: boolean
 }
 
-export function Input({
-  name,
-  skeleton,
-  fullWidth,
-  required,
-  mask = '',
-  label,
-  value,
-  error,
-  type,
-  disabled,
-  className,
-  skeletonClassName,
-  ...props
-}: IInput) {
-  if (skeleton)
-    return (
-      <div className={`skeleton ${fullWidth && 'w-full'} h-14 rounded-md ${skeletonClassName}`} />
-    )
+export const Input = forwardRef<HTMLInputElement, IInput>(
+  (
+    {
+      name,
+      skeleton,
+      fullWidth,
+      required,
+      mask = '',
+      label,
+      value,
+      error,
+      type,
+      disabled,
+      className,
+      skeletonClassName,
+      ...props
+    },
+    ref
+  ) => {
+    if (skeleton)
+      return (
+        <div className={`skeleton ${fullWidth && 'w-full'} h-14 rounded-md ${skeletonClassName}`} />
+      )
 
-  return (
-    <div className="relative flex flex-col">
-      <InputMask
-        mask={mask}
-        disabled={disabled}
-        type={type}
-        className={`peer border-2 rounded-md placeholder-transparent disabled:opacity-40 outline-none ${className}
+    return (
+      <div className="relative flex flex-col">
+        <InputMask
+          inputRef={ref}
+          mask={mask}
+          disabled={disabled}
+          type={type}
+          className={`peer border-2 rounded-md placeholder-transparent disabled:opacity-40 outline-none ${className}
         ${
           error
             ? 'border-error'
             : 'border-input-background focus:border-primary focus:ring-1 focus:ring-primary'
         } bg-input-background px-5 pt-5 pb-2 
         ${fullWidth && 'w-full'} `}
-        name={name}
-        value={value}
-        placeholder=""
-        {...props}
-      />
-      <label
-        className={`absolute text-common-grey20 top-1 left-5 text-xs transition-all pointer-events-none peer-placeholder-shown:text-base peer-placeholder-shown:top-4 peer-focus:top-1 peer-focus:text-xs`}>
-        {required ? `${label} *` : label}
-      </label>
-      {error && (
-        <div className="label-text-alt my-2 text-error">
-          {`O campo ${name} é obrigatório!` || ''}
-        </div>
-      )}
-    </div>
-  )
-}
+          name={name}
+          value={value}
+          placeholder=""
+          {...props}
+        />
+        <label
+          className={`absolute text-common-grey20 top-1 left-5 text-xs transition-all pointer-events-none peer-placeholder-shown:text-base peer-placeholder-shown:top-4 peer-focus:top-1 peer-focus:text-xs`}>
+          {required ? `${label} *` : label}
+        </label>
+        {error && (
+          <div className="label-text-alt my-2 text-error">
+            {`O campo ${name} é obrigatório!` || ''}
+          </div>
+        )}
+      </div>
+    )
+  }
+)
