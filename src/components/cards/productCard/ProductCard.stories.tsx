16c5b2cf11cba8a00f239568d7b55b838293e4c2
Jose Fortes <vasco0007@gmail.com>
2024-07-29 09:41:46 -0300

    update version

--- src/components/cards/productCard/ProductCard.stories.tsx
+++ src/components/cards/productCard/ProductCard.stories.tsx
@@ -12,7 +12,7 @@ export default meta
 type Story = StoryObj<typeof ProductCard>
 
 const urlImg =
-  'https://relaxaeusouti.com.br/wp-content/uploads/2020/11/Curso-Programador-FullStack-JavaScript-em-8-Semanas-e%CC%81-Bom-Vale-pena.jpeg'
+  'https://images.unsplash.com/photo-1484417894907-623942c8ee29?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 export const Default: Story = {
   args: {
