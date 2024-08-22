6b5b77636386de8114edb50a32a2adf2fe8d4e7f
Nalbert de Jesus <94402903+NJesus144@users.noreply.github.com>
2024-08-22 13:55:42 -0300

    Ds 90 fix modal (#59)
    * restore initial state
    
    * resolve conflicts
    
    * add confirm button functionality to  ModalWithButton component
    
    * add className prop to ModalWithButton component
    
    * update modal with button function

--- src/components/actions/modalWithButton/ModalWithButton.tsx
+++ src/components/actions/modalWithButton/ModalWithButton.tsx
@@ -8,6 +8,8 @@ interface IModalWithButton {
   skeleton?: boolean
   skeletonClassName?: string
   className?: string
+  confirmButtonText?: string
+  onConfirm?: () => void
 }
 
 export function ModalWithButton({
@@ -15,17 +17,31 @@ export function ModalWithButton({
   children,
   skeleton,
   skeletonClassName,
-  className
+  className,
+  confirmButtonText,
+  onConfirm
 }: IModalWithButton) {
   const [isOpen, setIsOpen] = useState(false)
   if (skeleton) return <div className={`skeleton w-32 h-12 rounded-lg ${skeletonClassName}`}></div>
 
+  const handleConfirm = () => {
+    onConfirm && onConfirm()
+    setIsOpen(false)
+  }
+
   return (
-    <>
-      <Button onClick={() => setIsOpen(true)}>{buttonText}</Button>
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} className={className}>
+    <div className={`${className}`}>
+      <Button fullWidth onClick={() => setIsOpen(true)}>
+        {buttonText}
+      </Button>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         {children}
+        {confirmButtonText && (
+          <Button onClick={handleConfirm} fullWidth>
+            {confirmButtonText}
+          </Button>
+        )}
       </Modal>
-    </>
+    </div>
   )
 }
