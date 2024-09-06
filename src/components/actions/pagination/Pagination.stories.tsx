1e659f764e91c530b7138fc30ca4498b90a6ddc1
Jose Fortes <jose@seja.dev>
2024-09-06 09:49:53 -0300

    update package

--- src/components/actions/pagination/Pagination.stories.tsx
+++ src/components/actions/pagination/Pagination.stories.tsx
@@ -72,3 +72,108 @@ export const LastPage: Story = {
     )
   }
 }
+
+export const OnlyOnePage: Story = {
+  render: ({ ...args }) => {
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const handlePageChange = (page: number) => {
+      setCurrentPage(page)
+    }
+
+    return (
+      <div className="flex flex-col gap-2">
+        <Pagination
+          {...args}
+          currentPage={currentPage}
+          totalPages={1}
+          onChange={handlePageChange}
+        />
+      </div>
+    )
+  }
+}
+
+export const TwoPages: Story = {
+  render: ({ ...args }) => {
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const handlePageChange = (page: number) => {
+      setCurrentPage(page)
+    }
+
+    return (
+      <div className="flex flex-col gap-2">
+        <Pagination
+          {...args}
+          currentPage={currentPage}
+          totalPages={2}
+          onChange={handlePageChange}
+        />
+      </div>
+    )
+  }
+}
+
+export const ThreePages: Story = {
+  render: ({ ...args }) => {
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const handlePageChange = (page: number) => {
+      setCurrentPage(page)
+    }
+
+    return (
+      <div className="flex flex-col gap-2">
+        <Pagination
+          {...args}
+          currentPage={currentPage}
+          totalPages={3}
+          onChange={handlePageChange}
+        />
+      </div>
+    )
+  }
+}
+
+export const FourPages: Story = {
+  render: ({ ...args }) => {
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const handlePageChange = (page: number) => {
+      setCurrentPage(page)
+    }
+
+    return (
+      <div className="flex flex-col gap-2">
+        <Pagination
+          {...args}
+          currentPage={currentPage}
+          totalPages={4}
+          onChange={handlePageChange}
+        />
+      </div>
+    )
+  }
+}
+
+export const FivePages: Story = {
+  render: ({ ...args }) => {
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const handlePageChange = (page: number) => {
+      setCurrentPage(page)
+    }
+
+    return (
+      <div className="flex flex-col gap-2">
+        <Pagination
+          {...args}
+          currentPage={currentPage}
+          totalPages={5}
+          onChange={handlePageChange}
+        />
+      </div>
+    )
+  }
+}
